46c97f7ec165bf14c6c74c6aece4230b4b56d9b2
Alexis Barnes <barnesan@live.unc.edu>
2016-02-22 19:40:51 -0500

    Added files via upload

--- project_2/js/script.js
+++ project_2/js/script.js
@@ -1,32 +1,36 @@
+$(document).ready(function(){
+  console.info("DOM Ready!")
+  $(".main").onepage_scroll({
+    sectionContainer: "section",
+    easing: "ease",
 
-$("main.onepage_scroll")({
-  sectionContainer: "section",
-  easing: "ease",
-
-  animationTime: 1000,
-  pagination: true,
-  updateURL: false,
-  beforeMove: function(index) {},
-  afterMove: function(index) {},
-  loop: false,
-  keyboard: true,
-  responsiveFallback: false,
-
-
-  direction: "vertical"
-});
-
- 
-$(window).scroll(function(){
-  afterMove: function(index) {
-      console.log("scroll element")
+    animationTime: 1000,
+    pagination: true,
+    updateURL: false,
+    beforeMove: function(index) {},
+    afterMove: function(index) {
+      console.log("afterMove");
+      console.log("scroll element");
       console.log(index);
       if (index == 2) {
         console.log("animating the image for second slide.");
-      
+
       $('#object').addClass("fadeIn");
-		
-   
+
+
       }
-    }
+
+      if (index == 3) {
+        console.log("animating the image for third slide.");
+
+        $('#wbb-graphic').addClass("slideRight");
+        $('#mbb-graphic').addClass("slideLeft");
+      }
+    },
+    loop: false,
+    keyboard: true,
+    responsiveFallback: false,
+
+    direction: "vertical"
+  });
 });
